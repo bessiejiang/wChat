45e97997446d15176e5090417be4aa4b7f0f94cf
Yangyifan Wang <ywang20@mail.sfsu.edu>
2020-05-15 15:27:38 -0700

    Add recent chat table view cell
    Add recent chat table view cell

---
+++ wChat/CustomCells/RecentChatsTableViewCell.swift
@@ -0,0 +1,90 @@
+//
+//  RecentChatsTableViewCell.swift
+//  wChat
+//
+//  Created by Kim Wang on 3/31/20.
+//  Copyright © 2020 Kim Wang. All rights reserved.
+//
+
+import UIKit
+protocol RecentChatsTableViewCellDelegate {
+    func didTapAvatarImage(indexPath: IndexPath)
+}
+
+class RecentChatsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lastMessageLabel: UILabel!
+    @IBOutlet weak var messageCounter: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var messageCounterBackgroundView: UIView!
+    
+    var indexPath : IndexPath!
+    let tapGesture = UITapGestureRecognizer()
+    var delegate : RecentChatsTableViewCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageCounterBackgroundView.layer.cornerRadius = messageCounterBackgroundView.frame.width / 2
+        
+        //add tap gesture
+        tapGesture.addTarget(self, action: #selector(self.avatarTap))
+        avatarImageView.isUserInteractionEnabled = true
+        avatarImageView.addGestureRecognizer(tapGesture)
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    //Mark: generate cell
+    
+    func generateCell(recentChat:NSDictionary, indexPath: IndexPath) {
+        self.indexPath = indexPath
+        self.nameLabel.text = recentChat[kWITHUSERFULLNAME] as? String
+        self.lastMessageLabel.text = recentChat[kLASTMESSAGE]  as? String
+        self.messageCounter.text = recentChat[kCOUNTER] as? String
+        
+        if let avatarString = recentChat[kAVATAR] {
+            imageFromData(pictureData: avatarString as! String) {(avatarImage) in
+                if avatarImage != nil {
+                    self.avatarImageView.image = avatarImage!.circleMasked
+                } else {
+                    
+                }
+            }
+        }
+        
+        if recentChat[kCOUNTER] as! Int != 0 {
+            self.messageCounter.text = "\(recentChat[kCOUNTER] as! Int)"
+            self.messageCounterBackgroundView.isHidden = false
+            self.messageCounter.isHidden = false
+        } else {
+            self.messageCounterBackgroundView.isHidden = true
+            self.messageCounter.isHidden = true
+        }
+        
+        var date : Date!
+        if let created = recentChat[kDATE] {
+            if (created as! String).count != 14 {//date saved in firebase is 14 digit long
+                date = Date()
+            } else {
+                date = DateFormatter().date(from: created as! String)!
+            }
+        } else {
+            date = Date()
+        }
+        
+        self.dateLabel.text = timeElapsed(date: date)
+    }
+    
+    @objc func avatarTap(){
+        print("avatar tapped")
+        delegate?.didTapAvatarImage(indexPath: indexPath)
+    }
+}
--- wChat/MainViews/ChatsViewController.swift
+++ wChat/MainViews/ChatsViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FirebaseFirestore
+class ChatsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {//the last two are protocols
 
-class ChatsViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var recentChats : [NSDictionary] = []
+    var filteredChats: [NSDictionary] = []
+    
+    var recentListener: ListenerRegistration! // used to listen for new changes
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
+        loadRecentChats()
         // Do any additional setup after loading the view.
     }
     
@@ -22,4 +29,50 @@ class ChatsViewController: UIViewController {
     }
     
 
+    //MARK: tableViewDateSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("here!")
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RecentChatsTableViewCell
+        //setup our cell
+        
+        
+        
+        
+        return cell
+    }
+    
+    //MARK: LoadRecentChats
+    func loadRecentChats() {
+        recentListener = reference(.Recent).whereField(kUSERID, isEqualTo: FUser.currentId()).addSnapshotListener({(snapshot, error) in
+            
+            guard let snapshot = snapshot else { return }
+            
+            self.recentChats = []
+            
+            if !snapshot.isEmpty {
+                //sort recent obj based on date
+                let sorted = ((dictionaryFromSnapshots(snapshots: snapshot.documents)) as NSArray).sortedArray(using: [NSSortDescriptor(key: kDATE, ascending: false)])  as! [NSDictionary]//decending, latest one on top
+                
+                for recent in sorted {
+                    if recent[kLASTMESSAGE] as! String != "" && recent[kCHATROOMID] != nil && recent[kRECENTID] != nil {
+                        
+                        self.recentChats.append(recent)
+                    }
+                    
+                    reference(.Recent).whereField(kCHATROOMID, isEqualTo: recent[kCHATROOMID] as! String).getDocuments(completion: { (snapshot, error) in
+                        
+                    })
+                }
+                
+                self.tableView.reloadData()
+            }
+           
+        })
+        
+        
+        
+    }
 }
